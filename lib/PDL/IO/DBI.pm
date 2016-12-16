ae3ac769f025e781e276ac8c89da9577a76a1216
kmx <kmx@cpan.org>
2016-12-16 19:57:18 +0100

    v0.011

--- lib/PDL/IO/DBI.pm
+++ lib/PDL/IO/DBI.pm
@@ -7,7 +7,7 @@ use Exporter 'import';
 our @EXPORT_OK   = qw(rdbi1D rdbi2D);
 our %EXPORT_TAGS = (all => \@EXPORT_OK);
 
-our $VERSION = '0.010';
+our $VERSION = '0.011';
 
 use Config;
 use constant NO64BITINT => ($Config{ivsize} < 8) ? 1 : 0;
